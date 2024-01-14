c1ba663eb3d4773699e76e1edfffb785863d3823
Jung-10 <67498182+Jung-10@users.noreply.github.com>
2024-01-15 02:08:14 +0900

    Title: 약수 출력하기, Time: 112ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240115/약수 출력하기/print-divisor.py
+++ 240115/약수 출력하기/print-divisor.py
@@ -1,9 +1,20 @@
 n = int(input())
 answer = []
 
-for i in range(1, n + 1) :
+# 방법 1 => 시간초과
+# for i in range(1, n + 1) :
+#     if n % i == 0 :
+#         answer.append(i)
+
+# for j in answer :
+#     print(j, end = " ")
+
+for i in range(1, int(round(n**(1/2), 0)) + 1) :
     if n % i == 0 :
         answer.append(i)
+        answer.append(n // i)
+
+answer = sorted(answer)
 
 for j in answer :
     print(j, end = " ")

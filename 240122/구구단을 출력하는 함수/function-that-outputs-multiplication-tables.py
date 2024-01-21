5362a1329dbece27fb913d5789be50df2e5fe1ff
Jung-10 <67498182+Jung-10@users.noreply.github.com>
2024-01-22 00:31:05 +0900

    Title: 구구단을 출력하는 함수, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240122/구구단을 출력하는 함수/function-that-outputs-multiplication-tables.py
+++ 240122/구구단을 출력하는 함수/function-that-outputs-multiplication-tables.py
@@ -14,20 +14,19 @@
 #         print('{0} * {1} = {2}'.format(i, j, i * j))
 
 # 방법 2 :
-# 1. 받은 숫자 리스트에 넣어서 정렬
-# 2. 3개 중 2번째 숫자와 같지 않으면 result 리스트에 넣음
-# 3. result 리스트 안의 값들을 넣어서 구구단 출력
+# 함수 사용
+
+def print_multiplication_table(exclude_value, start, end):
+    for i in range(start, end + 1):
+        if i == exclude_value:
+            continue
+        for j in range(1, 10):
+            print('{0} * {1} = {2}'.format(i, j, i * j))
+
 x, y, z = map(int, input().split(' '))
-
 xyz_list = [x, y, z]
-xyz_list = sorted(xyz_list)
+xyz_list.sort()
 
-result = []
+exclude_value = xyz_list[1]
 
-for i in range(xyz_list[0], xyz_list[2] + 1) :
-    if i != xyz_list[1] :
-        result.append(i)
-
-for i in result :
-    for j in range(1, 10) :
-        print('{0} * {1} = {2}'.format(i, j, i * j))
+print_multiplication_table(exclude_value, xyz_list[0], xyz_list[2])

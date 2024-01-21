13bf032a0f9a8b7ef6ca2d192602a80717c8609c
Jung-10 <67498182+Jung-10@users.noreply.github.com>
2024-01-22 00:18:32 +0900

    Title: 구구단을 출력하는 함수, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240122/구구단을 출력하는 함수/function-that-outputs-multiplication-tables.py
@@ -0,0 +1,13 @@
+# 1. 받은 숫자 리스트에 넣어서 정렬
+# 2. 3개 중 2번째 숫자와 같으면 출력하지않고 나머지는 출력
+
+x, y, z = map(int, input().split(' '))
+
+xyz_list = [x, y, z]
+xyz_list = sorted(xyz_list)
+
+for i in range(xyz_list[0], xyz_list[2] + 1) :
+    if i == xyz_list[1] :
+        continue
+    for j in range(1, 10) :
+        print('{0} * {1} = {2}'.format(i, j, i * j))

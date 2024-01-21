5d37d027ba055b6c991da56505deb313c9cb77ef
Jung-10 <67498182+Jung-10@users.noreply.github.com>
2024-01-22 01:55:50 +0900

    Title: 연달아 출력하는 재귀함수, Time: 1028ms, Memory: 288MB, Status: Memory Limit Exceed - CodeTree

--- 240122/연달아 출력하는 재귀함수/recursive-function-that-outputs-continuously.py
+++ 240122/연달아 출력하는 재귀함수/recursive-function-that-outputs-continuously.py
@@ -1,3 +1,4 @@
+# 방법 1 : 메모리 초과 - 리스트 사용보다 필요한 값을 직접 계산하거나 저장하는 방식으로 코드 변경
 n = int(input())
 
 result_list = [n]
@@ -10,3 +11,12 @@ result_list.reverse()
 
 for i in result_list :
     print(i, end = ' ')
+
+
+# n = int(input())
+
+# while n != 1:
+#     print(n, end=' ')
+#     n = n // 3
+
+# print(1, end=' ')

e5209ed6a35bff1d9c52f3740c8419308a0ee81b
Jung-10 <67498182+Jung-10@users.noreply.github.com>
2024-01-26 00:57:40 +0900

    Title: 제곱하여 출력하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240126/제곱하여 출력하기/print-square-of-elements.py
+++ 240126/제곱하여 출력하기/print-square-of-elements.py
@@ -3,5 +3,7 @@ num_list = list(map(int, input().split(' ')))
 
 result = [i ** 2 for i in num_list]
 
-for i in result :
-    print(i, end = ' ')
+print(*result)
+
+# *은 unpacking 연산자(asterisk)
+# 리스트나 튜플과 같은 iterable 객체의 요소를 풀어서 전달 가능

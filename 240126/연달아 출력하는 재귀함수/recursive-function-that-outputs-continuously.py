050df6d1635998d9e2c5c060d9df077f1aaf666c
Jung-10 <67498182+Jung-10@users.noreply.github.com>
2024-01-26 02:00:13 +0900

    Title: 연달아 출력하는 재귀함수, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240126/연달아 출력하는 재귀함수/recursive-function-that-outputs-continuously.py
+++ 240126/연달아 출력하는 재귀함수/recursive-function-that-outputs-continuously.py
@@ -1,10 +1,16 @@
 n = int(input())
-result_list = [n]
 
-while n != 1 :
-    n = n // 3
-    result_list.append(n)
+arr = [n]
 
-reversed_list = list(reversed(result_list))
+# 재귀함수
+def factorial(n) :
+    if n != 1 :
+        n = n // 3
+        arr.append(n)
+        factorial(n)
 
+result = factorial(n)
+
+# 정수 n에서 시작하여 수를 왼쪽으로 이동하며 수를 하나씩 적어야하는데 오른쪽으로 이동해서 출력했으므로 뒤집어주기
+reversed_list = list(reversed(arr)) 
 print(*reversed_list)

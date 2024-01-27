4d1062f538c36863d46364a4d33df0b13a85e12c
Jung-10 <67498182+Jung-10@users.noreply.github.com>
2024-01-28 00:35:13 +0900

    Title: 파스칼의 삼각형, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240128/파스칼의 삼각형/pascal's-triangle.py
+++ 240128/파스칼의 삼각형/pascal's-triangle.py
@@ -9,12 +9,9 @@ arr[0][0] = 1
 arr[1][0] = 1
 arr[1][1] = 1
 
-# 첫 번째 열은 모두 1로 초기화
+# 첫 번째 열은 모두 1로 초기화, 마지막 열 모두 1로 초기화
 for i in range(n):
     arr[i][0] = 1
-
-# 마지막 열 모두 1로 초기화
-for i in range(n) :
     arr[i][i] = 1
 
 for i in range(2, n) :

1bc9fdc48af54336c23d88896f148948ccf3dfe6
Jung-10 <67498182+Jung-10@users.noreply.github.com>
2024-02-15 16:27:03 +0900

    Title: 블럭쌓는 명령2, Time: 126ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240215/블럭쌓는 명령2/block-stacking-commands2.py
@@ -0,0 +1,10 @@
+n, k = map(int, input().split())
+
+color = [0 for _ in range(n + 1)]
+
+for _ in range(k) :
+    a, b = map(int, input().split())
+    for i in range(a, b + 1) :
+        color[i] += 1
+
+print(max(color))

158edc6c2ca43cddbcc8acdf37eb41c0b7e11f11
Jung-10 <67498182+Jung-10@users.noreply.github.com>
2024-02-15 15:50:56 +0900

    Title: 2진수로 변환하기, Time: 110ms, Memory: 24MB, Status: Passed - CodeTree

--- 240215/2진수로 변환하기/convert-to-binary.py
+++ 240215/2진수로 변환하기/convert-to-binary.py
@@ -2,11 +2,13 @@ n = int(input())
 
 answer = []
 
-while n != 1 :
+while True :
+    if n < 2 :
+        answer.append(n)
+        break
+    
     answer.append(n % 2)
     n = n // 2
-
-answer.append(1)
 
 for elem in answer[::-1] :
     print(elem, end='')

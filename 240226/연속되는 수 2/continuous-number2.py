7dee62fe20682a2fcc5594fd6ec64b00e9163e0e
Jung-10 <67498182+Jung-10@users.noreply.github.com>
2024-02-26 19:44:02 +0900

    Title: 연속되는 수 2, Time: 133ms, Memory: 24MB, Status: Passed - CodeTree

--- 240226/연속되는 수 2/continuous-number2.py
+++ 240226/연속되는 수 2/continuous-number2.py
@@ -5,15 +5,15 @@ arr = []
 for _ in range(n) :
     arr.append(int(input()))
 
-cnt = 1
+cnt = 0
 max_cnt = 0
 for i in range(1, n) :
     if arr[i] == arr[i-1] :
         cnt += 1
     else :
         max_cnt = max(cnt, max_cnt)
-        cnt = 1
+        cnt = 0
     
     max_cnt = max(cnt, max_cnt)
 
-print(max_cnt)
+print(max_cnt + 1)

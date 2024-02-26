c7e2e37dd14e5438ef3034b9834993ca92f6f582
Jung-10 <67498182+Jung-10@users.noreply.github.com>
2024-02-26 20:04:45 +0900

    Title: 연속되는 수 3, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240226/연속되는 수 3/continuous-number3.py
+++ 240226/연속되는 수 3/continuous-number3.py
@@ -5,14 +5,14 @@ arr = [
 	for _ in range(n)
 ]
 
-cnt = 0
+cnt = 1
 result = 0
 for i in range(n) :
     if (arr[i] > 0 and arr[i-1] > 0) or (arr[i] < 0 and arr[i-1] < 0) and i >= 1 :
         cnt += 1
     else :
-        cnt = 0
+        cnt = 1
     
     result = max(cnt, result)
 
-print(result + 1)
+print(result)

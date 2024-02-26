003f17039e87232a4407f5bb1f1818fef0ed4c15
Jung-10 <67498182+Jung-10@users.noreply.github.com>
2024-02-26 19:40:59 +0900

    Title: 연속되는 수 2, Time: 102ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240226/연속되는 수 2/continuous-number2.py
+++ 240226/연속되는 수 2/continuous-number2.py
@@ -8,7 +8,11 @@ for _ in range(n) :
 cnt = 0
 max_cnt = 0
 for i in range(n) :
-    if arr[i] != arr[i-1] or i == 0 :
+    if arr[i] == arr[i-1] or i == 0 :
         cnt += 1
+    else :
+        cnt += 1
+        max_cnt = max(cnt, max_cnt)
+        cnt = 0
 
-print(cnt)
+print(max_cnt)

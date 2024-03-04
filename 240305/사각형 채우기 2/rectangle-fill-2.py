02d2405f40ce248afc936929cf5bc4aa497d93a7
Jung-10 <67498182+Jung-10@users.noreply.github.com>
2024-03-05 00:37:29 +0900

    Title: 사각형 채우기 2, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240305/사각형 채우기 2/rectangle-fill-2.py
+++ 240305/사각형 채우기 2/rectangle-fill-2.py
@@ -2,10 +2,13 @@ n = int(input())
 mod = 10007
 
 dp = [0] * 1001
+dp[0] = 1
 dp[1] = 1
 dp[2] = 3
 
 for i in range(3, n + 1) :
-    dp[i] = i + i-1
+    dp[i] = dp[i - 1] + dp[i - 2]
+    for j in range(i - 3, -1, -1) :
+        dp[i] = dp[i] + 2 * dp[j]
 
-print(dp[i] % mod)
+print(dp[n] % mod)

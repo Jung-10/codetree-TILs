ae4a8866ecdcfdb668bd8254cd6217db260b0831
Jung-10 <67498182+Jung-10@users.noreply.github.com>
2024-03-05 00:27:42 +0900

    Title: 사각형 채우기 3, Time: 102ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240305/사각형 채우기 3/rectangle-fill-3.py
+++ 240305/사각형 채우기 3/rectangle-fill-3.py
@@ -2,11 +2,10 @@ n = int(input())
 mod = 1000000007
 
 dp = [0] * 1001
+dp[0] = 1 # 타일을 전혀 놓지 않는 경우가 한 가지 있으므로 의미상 1
 dp[1] = 2
-dp[2] = 7
-dp[3] = 22
 
-for i in range(4, n + 1) :
-    dp[i] = (dp[i - 1] * 4) - (dp[i - 2] * 3)
+for i in range(2, n + 1) :
+    dp[i] = (dp[i - 1] * 2) + (dp[i - 2] * 3)
 
 print(dp[n] % mod)

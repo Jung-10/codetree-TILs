57d06ff9ef34c8b77f0892b004c6ea0092335409
Jung-10 <67498182+Jung-10@users.noreply.github.com>
2024-03-04 23:18:25 +0900

    Title: 피보나치 수, Time: 5091ms, Memory: 28MB, Status: Time Limit Exceed - CodeTree

--- 240304/피보나치 수/fibonacci-number.py
+++ 240304/피보나치 수/fibonacci-number.py
@@ -1,15 +1,15 @@
-def fibbo(n, memo) :
+def fibbo(n) :
     if memo[0] != -1 :
         return memo[n]
     
     if n <= 2 :
         memo[n] = 1
     else :
-        memo[n] = fibbo(n - 1, memo) + fibbo(n - 2, memo)
+        memo[n] = fibbo(n - 1) + fibbo(n - 2)
 
     return memo[n]
 
 n = int(input())
 memo = [-1] * (n + 1)  # 메모이제이션을 위한 배열 초기화
-result = fibbo(n, memo)
+result = fibbo(n)
 print(result)
